ff8cf71c12f2c4932d55d0e2b2f965c5b8d2b165
Helder <helder@lamoulade.com>
2015-07-09 18:29:42 +0100

    some work done to solve issue related with current user Y position and matching breakpoints. seems to work now.

--- js/app-kirupa-tutorial.js
+++ js/app-kirupa-tutorial.js
@@ -26,6 +26,13 @@ RAFParallax.prototype = {
 		this.transformProperty = this.getSupportedPropertyName(this.transforms);
 		this.container = document.querySelector(params.containerElement);
 		this.scrollWrap = document.querySelector(params.scrollWrapElement);
+		this.eased_scroll_y = 0;
+		this.ease = 0.25;
+		this.containerChildElementNodeList = document.querySelectorAll(params.containerChildClassName);
+		this.panels = this.getNodeListToArray(this.containerChildElementNodeList);
+		this.breakpoints = {};
+		this.breakpointCallbacks = params.breakpointCallbacks;
+		this.setBreakpointPositions();
 
 		this.listeners();
 
@@ -85,30 +92,35 @@ RAFParallax.prototype = {
 
 	getScrollPosition: function () {
 
+		var y = 0;
+
 		if (document.documentElement.scrollTop === 0) {
 
-			return document.body.scrollTop;
+			y = document.body.scrollTop;
 
 		} else {
 
-			return document.documentElement.scrollTop;
+			y = document.documentElement.scrollTop;
 
 		}
 
+		this.eased_scroll_y += ( (y) - this.eased_scroll_y) * this.ease;
+
+		return this.eased_scroll_y;
 	},
 
 	setTranslate3DTransform: function  (element, yPosition) {
 		var value = "translate3d(0px" + ", " + yPosition + "px" + ", 0)";
 		element.style.webkitTransform = value;
+		this.yPosition = yPosition;
 	},
 
 	loop: function () {
 
-		// adjust the image's position when scrolling
-		if (this.scrolling) {
-			this.setTranslate3DTransform(this.scrollWrap, -1 * this.getScrollPosition() / 2);
-			this.scrolling = false;
-		}
+		var yPosition = this.getScrollPosition();
+
+		// adjust scroll wrap when scrolling
+		this.setTranslate3DTransform(this.scrollWrap, yPosition / 2);
 
 		// scroll up or down by 10 pixels when the mousewheel is used
 		if (this.mouseWheelActive) {
@@ -123,11 +135,69 @@ RAFParallax.prototype = {
 			}
 		}
 
+		this.breakpointCallbackChecker({
+			top: this.yPosition,
+			left: 0
+		});
+
 	    requestAnimationFrame(this.loop.bind(this));
 
 	},
 
+	setBreakpointPositions: function () {
 
+		this.panels.forEach(function (panel, index) {
+
+			var panelName = panel.getAttribute('data-breakpoint');
+
+			this.breakpoints[panelName] = {
+
+				pos: {
+					top: panel.offsetTop,
+					bottom: panel.offsetTop + panel.offsetHeight
+				},
+
+				callback: typeof this.breakpointCallbacks[panelName] === "function" ? this.breakpointCallbacks[panelName] : null,
+
+				width: panel.offsetWidth,
+
+				height: panel.offsetHeight,
+
+				el: panel
+
+			};
+
+		}.bind(this));
+
+	},
+
+	breakpointCallbackChecker: function (params) {
+
+		var bp = null;
+
+		for (var k in this.breakpoints) {
+
+			bp = this.breakpoints[k];
+
+			if (params.top >= bp.pos.top && params.top <= bp.pos.bottom) {
+
+				if (typeof bp.callback === "function") {
+
+					bp.callback(this);
+
+				}
+
+			}
+
+		}
+
+	},
+
+	getNodeListToArray: function (nodeList) {
+
+		return [].slice.call(nodeList);
+
+	}
 
 };
 
